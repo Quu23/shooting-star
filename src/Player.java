db45965281024ff5f83559a257ce677d65798f2f
ryota <mike.maro0810@gmail.com>
2022-10-30 22:24:17 +0900

    訂正した

--- src/Player.java
+++ src/Player.java
@@ -1,32 +1,29 @@
 public class Player{
     int hp = 10;
     int lv = 1;
+    int maxHp = 10;
     
-    public void lv(int lv,int score,boolean breakEnemy){
-        if(breakEnemy){
-            score+=10;
-            breakEnemy=false;
-        }
+    public void lvUp(int lv,int score,boolean breakEnemy){
+        score+=10;
+        breakEnemy=false;
 
-        int tscore = score;
-        if(score==(tscore+100)){
+        int tLv = lv;
+
+        if(score%100==0){
             lv++;
+            if((tLv+1)==lv){
+                maxHp+=5;
+            }
         }
-    }
 
-    public void hp(int lv,int maxHp){
-        int tlv = lv;
-        if(lv==(tlv+1)){
-            maxHp+=5;
-        }
     }
 
     public void attack(){
 
     }
 
-    public void heal(int hp, boolean healOrb){
-        if(healOrb){
+    public void healUp(int hp, boolean isGetHealOrb){
+        if(isGetHealOrb){
             hp++;
         }
     }
