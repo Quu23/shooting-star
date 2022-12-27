eff82d03fddc8ebd6001fcac24f707c47625284e
Quu <91326783+Quu23@users.noreply.github.com>
2022-12-27 21:21:17 +0900

    プレイヤーのx,y座標を保持させた。

--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,16 @@ public class Player{
     int hp = 10;
     int lv = 1;
     int maxHp = 10;
+
+    //プレイヤーの座標
+    int x;
+    int y;
+
+    //コンストラクタ
+    Player(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
     
     public void lvUp(int lv,int score,boolean breakEnemy){
         score+=10;
