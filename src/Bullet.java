855c5b24e10fef0dc9a598c4faffc08332985c09
ryota <mike.maro0810@gamail.com>
2023-02-18 23:53:56 +0900

    angleつけた

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,7 +1,7 @@
 public abstract class Bullet extends Entity{
-    
+    double angle;
 
-    Bullet(int x,int y,int width,int height){
+    Bullet(int x,int y,int width,int height,int angle){
         super(x,y,width,height);
         this.angle=angle;
     }
