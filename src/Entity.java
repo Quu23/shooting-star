f736b348622e4ec747d9f78d90b7799fe83a96c6
Quu <91326783+Quu23@users.noreply.github.com>
2023-02-18 22:32:28 +0900

    幅と高さの変数を追加した。

--- src/Entity.java
+++ src/Entity.java
@@ -1,10 +1,12 @@
 public abstract class Entity {
-    int x,y;
+    int x,y,width,height;
 
-    Entity(int x,int y){
+    Entity(int x,int y,int width,int height){
         this.x=x;
         this.y=y;
+        this.width=width;
+        this.height=height;
     }
 
-    
+
 }
