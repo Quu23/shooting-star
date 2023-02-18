6ae2b093c283cb76391a8d6b527b6b32e7d16cc5
Quu <91326783+Quu23@users.noreply.github.com>
2023-02-18 23:11:33 +0900

    対象のエンティティを当たっているかどうかを判定できるようにした。

--- src/Entity.java
+++ src/Entity.java
@@ -1,4 +1,6 @@
 public abstract class Entity {
+
+    // x,yはエンティティの中心の座標　w,hは円で言うところの半径
     int x,y,width,height;
 
     Entity(int x,int y,int width,int height){
@@ -8,5 +10,19 @@ public abstract class Entity {
         this.height=height;
     }
 
+    // 対象のエンティティと接しているかどうか。
+    public boolean isHit(Entity target) {
+
+        double angle = Math.atan2(this.y-target.y, this.x-target.x);
+
+        double square_distance = 
+        (this.x-target.x)*(this.x-target.x)+(this.y-target.y)*(this.y-target.y)
+        -((this.width/Math.cos(angle))*(this.width/Math.cos(angle))+(target.width/Math.cos(angle))*(target.width/Math.cos(angle)));
+
+        if(square_distance<0){
+            return true;
+        }
+        return false;
+    }
 
 }
