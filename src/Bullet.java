f3ffb8582077a704c817db859389053f6e665452
Quu <91326783+Quu23@users.noreply.github.com>
2023-02-23 21:08:47 +0900

    equals()とhashCode()を実装した。

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Bullet extends Entity{
     //ラジアン
     double angle;
@@ -5,5 +7,22 @@ public class Bullet extends Entity{
     Bullet(int x,int y,int width,int height,double angle){
         super(x,y,width,height);
         this.angle=angle;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj==this)return true;
+        if(obj==null)return false;
+        if(!(obj instanceof Bullet))return false;
+        Bullet b=(Bullet)obj;
+        if(b.x==this.x&&b.y==this.y&&b.width==this.width&&b.height==this.height&&b.angle==this.angle){
+           return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.x,this.y,this.width,this.height,this.angle);
     }
 }
--- src/Plane.java
+++ src/Plane.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public abstract class Plane extends Entity{
 
@@ -13,5 +14,22 @@ public abstract class Plane extends Entity{
 
     public abstract void addBullet();
     public abstract void moveBullet();
+
+     @Override
+     public boolean equals(Object obj) {
+         if(obj==this)return true;
+         if(obj==null)return false;
+         if(!(obj instanceof Plane))return false;
+         Plane p=(Plane)obj;
+         if(p.x==this.x&&p.y==this.y&&p.width==this.width&&p.height==this.height&&p.power==this.power&&p.bullets.size()==this.bullets.size()){
+            return true;
+         }
+         return false;
+     }
+
+     @Override
+     public int hashCode() {
+         return Objects.hash(this.x,this.y,this.width,this.height,this.power,this.bullets);
+     }
     
 }
