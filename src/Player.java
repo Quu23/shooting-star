dc9631121a2452a1dd905409e0edac818fadc1d8
Quu <91326783+Quu23@users.noreply.github.com>
2023-03-09 22:06:07 +0900

    弾の感覚を調節した弾の感覚を調節した

--- src/Player.java
+++ src/Player.java
@@ -22,7 +22,7 @@ public class Player extends Plane{
         this.bulletCoolTime--;
         if(Main.isKeyPressed[0]&&this.bulletCoolTime<=0){
             this.addBullet();
-            this.bulletCoolTime=20;
+            this.bulletCoolTime=50;
         }
         if(Main.isKeyPressed[1]&&this.x-this.width>0)this.x-=this.MOVE_SPEED;
         if(Main.isKeyPressed[2]&&this.y-this.height>0)this.y-=this.MOVE_SPEED;
