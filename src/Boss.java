895636c2fdd5cdbe7b0a175da4cbf7befff27a4f
Quu <91326783+Quu23@users.noreply.github.com>
2023-03-23 22:02:06 +0900

    bossクラスとキャノンクラスを作った。

--- src/Boss.java
+++ src/Boss.java
@@ -2,13 +2,24 @@ import java.awt.Color;
 
 public class Boss extends Enemy{
 
+    int degreeCount=0;
+
+    Cannon cannons[] = {new Cannon(this.x-50,this.y+10),new Cannon(this.x-25,this.y+10),new Cannon(this.x+25,this.y+10),new Cannon(this.x+50,this.y+10)};
+
     Boss(int x,int y) {
         super(x, y, 5, 5, 3,5, 1000000, 1000, Color.black);
     }
 
     @Override
     public void addBullet() {
-        // todo
+        int intervalDegree=20;
+
+        if(this.degreeCount<7){
+            new Bullet(this.x,this.y+10,3,3,Math.toRadians(210+intervalDegree*this.degreeCount),this.MOVE_SPEED+2);
+        }else{
+            new Bullet(this.x,this.y+10,3,3,Math.toRadians(210+intervalDegree*(12-this.degreeCount)),this.MOVE_SPEED+2);
+        }
+        this.degreeCount%=12;
     }
     
 }
---
+++ src/Cannon.java
@@ -0,0 +1,16 @@
+public class Cannon extends StraightEnemy{
+
+    Cannon(double x, double y) {
+        super((int)x,(int)y, 5, 5);
+    }
+
+    @Override
+    public void action() {
+        this.bulletCoolTime--;
+        if(this.bulletCoolTime<=0){
+            this.addBullet();
+            this.bulletCoolTime=this.BULLET_COOL_TIME;
+        }
+    }
+    
+}
